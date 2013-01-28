bfa9545a9196243bad36702df6ce3a939b2f8fc9
Chad Metcalf <chad@databasically.com>
2013-01-28 11:38:54 -0600

    WIP

--- lib/omniauth/strategies/att_oauth2.rb
+++ lib/omniauth/strategies/att_oauth2.rb
@@ -14,54 +14,25 @@ module OmniAuth
       option :name, "att_oauth2"
       option :authorize_options, [:scope]
       option :short_code, nil
-      # option :state
 
       option :client_options, {
         :site             => 'https://api.att.com',
         :authorize_url    => '/oauth/authorize',
         :token_url        => '/oauth/token',
+        :token_method     => :post,
         :endpoint_msisidn => "",
-        :raise_errors     => true
+        :raise_errors     => false,
       }
 
       option :token_params, {
         :parse => :json
       }
 
-      # These are called after authentication has succeeded. 
-      uid{ raw_info['uid'] }
-
-      info do
-        prune!({
-          :name               => raw_info['info']['name'],
-          :phone_number       => raw_info['info']['phone_number'],
-          :email              => raw_info['info']['email'],
-          :first_name         => raw_info['info']['first_name'],
-          :last_name          => raw_info['info']['last_name']
-        })
-      end
-
-      extra do
-        {
-          'raw_info' => raw_info
-        }
-      end
-
       def request_phase
-        Syslog.open($0, Syslog::LOG_PID | Syslog::LOG_CONS) { |s| s.warning "IN THE CUSTOM request" }
         options[:scope] ||= "SMS,MMS,TL,DC"
         options[:authorize_params][:response_type] ||= 'code'
-        options[:authorize_params][:state] = options[:state]
         super
       end
-      
-      #######################
-      # def request_phase
-      #   raise 
-      #   options[:scope] ||= "SMS,MMS,TL,DC"
-      #   redirect client.auth_code.authorize_url(
-      #     {:redirect_uri => callback_url, :response_type => "code"}.merge(options))
-      # end
 
     end
   end
